e76aacc00ed10b6fe780462e0c4034d0fe8a0212
Steeve Borgniet <djmrst.officiel@gmail.com>
2026-08-24 11:25:20 +0200

    Ajout du script pour les onglets

---
+++ script.js
@@ -0,0 +1,26 @@
+function switchTab(tabName) {
+    const tabContents = document.querySelectorAll('.tab-content');
+    const tabButtons = document.querySelectorAll('.tab-btn');
+
+    tabContents.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+
+    tabButtons.forEach(function(button) {
+        button.classList.remove('active');
+    });
+
+    const selectedTab = document.getElementById(tabName + 'Tab');
+
+    if (selectedTab) {
+        selectedTab.classList.add('active');
+    }
+
+    if (tabName === 'photos') {
+        tabButtons[0].classList.add('active');
+    }
+
+    if (tabName === 'videos') {
+        tabButtons[1].classList.add('active');
+    }
+}
